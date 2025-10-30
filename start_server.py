e6e5a44285f29338d9917fb651bb8e74e8aa98e5
Faisal Abubaker <ahmadfaisal47@gmail.com>
2025-10-30 08:16:33 +0000

    Add server log file to track application requests and responses

--- start_server.py
+++ start_server.py
@@ -7,4 +7,4 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 if __name__ == "__main__":
     # Run the FastAPI application
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8080, reload=True)
